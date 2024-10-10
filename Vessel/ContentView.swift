b2728c5f86f3b2eeade6870192464ac3bd2239ce
Hang Zhou <zhou_hang@berkeley.edu>
2024-10-10 11:19:22 -0400

    Initial Commit

---
+++ Vessel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vessel
+//
+//  Created by Zhou Hang on 10/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vessel/VesselApp.swift
@@ -0,0 +1,17 @@
+//
+//  VesselApp.swift
+//  Vessel
+//
+//  Created by Zhou Hang on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VesselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
